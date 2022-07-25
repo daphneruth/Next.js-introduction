30a1d24e2e9d3e035c76d1d50340242718fc83f4
Daphne <daphneruth304@gmail.com>
2022-07-25 10:59:27 +0300

    updated

--- mynext-app/pages/news/index.js
+++ mynext-app/pages/news/index.js
@@ -1,25 +1,18 @@
-   import Link from 'next/link';
-   import {Fragment} from 'react';
-   function NewsPage (){
-      return(
-         <Fragment>
-     <h1>The News Page</h1>
-     <ul>
-      <li>
-       <Link  >
-       NextJs Details
-       </Link>
-      </li>
-      <li>
-         <Link>
-         Other Related
-         </Link>
-        
-      </li>
-     </ul>
-      
-         </Fragment>
-      )
-        
-      }
-      export default NewsPage;
+import Link from "next/link";
+import { Fragment } from "react";
+function NewsPage() {
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link>NextJs Details</Link>
+        </li>
+        <li>
+          <Link>Other Related</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
+}
+export default NewsPage;
