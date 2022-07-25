d8925d3e6dc25ec90027fb13d1cda00b983f0020
Daphne <daphneruth304@gmail.com>
2022-07-25 10:47:03 +0300

    extractng dynamic parameters

--- mynext-app/pages/news/[newsId].js
+++ mynext-app/pages/news/[newsId].js
@@ -1,5 +1,9 @@
-
+import {useRouter} from 'next/router';
 function DetailPage (){
+
+   const router = useRouter();
+
+   const newsId = router.query.newsId;
     return(
        <h1>The Details Page</h1>
     )
